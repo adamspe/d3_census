f366251e269adb75714f70b6fcd94ec4ce7373a5
Paul Adams <adamspe@gmail.com>
2014-11-18 17:31:53 -0600

    add info bubble with controls to close and update the dataset.

--- js/app.js
+++ js/app.js
@@ -29,7 +29,25 @@ angular.module('cpi.d3',[
             .append('g');
         $scope.node = $scope.chart.selectAll(".node");
 
+        $scope.tooltip = d3.select('.cpi_info');
+        $scope.closeCpiInfo = function() {
+            $scope.tooltip.style("visibility", "hidden");
+        }
+        $scope.dropSelectedState = function() {
+            if($scope.cpiInfoState) {
+                for(var i = 0; i < $scope.data.selections.length; i++) {
+                    if($scope.data.selections[i].label === $scope.cpiInfoState) {
+                        $scope.data.selections[i].selected = false;
+                        $scope.closeCpiInfo();
+                        return $scope.visualize();
+                    }
+                }
+            }
+        }
+
+
         $scope.visualize = function() {
+            $scope.closeCpiInfo();
             var data = $scope.data.selections.reduce(function(prev,curr,idx,arr){
                 return (curr.selected && $scope.data.data[curr.label]) ?
                     prev.concat($scope.data.data[curr.label].data) : prev;
@@ -49,6 +67,13 @@ angular.module('cpi.d3',[
                         d3.max(data,function(d){return d[key];})])
                     .range([cpi.SMALLEST_R,cpi.BIGGEST_R]);
 
+            function setClass(d) {
+                var cls = "circle "+d.state;
+                if(r(d[key]) === cpi.BIGGEST_R) {
+                    cls += " max";
+                }
+                return cls;
+            }
             // update the data set
             $scope.node = $scope.node.data(data,function(d) { return d.urban_area; });
             // exit
@@ -76,7 +101,8 @@ angular.module('cpi.d3',[
                 .attr('r',function(d) { return d['current_radius']; })
                 .transition()
                 .duration(cpi.UPDATE_TRANS_DURATION)
-                .attr("r",function(d) { return (d['current_radius']=r(d[key])); });
+                .attr("r",function(d) { return (d['current_radius']=r(d[key])); })
+                .attr('class',setClass);
 
             // enter selection (grow circles from 0 to their designated size)
             var enter = $scope.node.enter().append("g")
@@ -85,7 +111,23 @@ angular.module('cpi.d3',[
                   .call($scope.force.drag); // allow the user to drag nodes around
 
             enter.append("circle")
-                    .attr("class",function(d) { return "circle "+d.state; })
+                    .attr("class",setClass)
+                    .on("mouseover", function(d){
+                        var html = '<h4>'+d.urban_area+'</h4>',key;
+                        html += '<ul class="list-unstyled">';
+                        for(key in $scope.data.labels) {
+                            html += '<li><label>'+$scope.data.labels[key]+':</label> '+d[key]+'%</li>'
+                        }
+                        html += '</ul>';
+                        $scope.$apply(function() {
+                            $scope.cpiInfoState = d.state;
+                        });
+                        $scope.tooltip.select('.cpi_info_body').html(html);
+                        $scope.tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");
+                        return $scope.tooltip.style("visibility", "visible");
+                    })
+                    //.on("mousemove", function(){return $scope.tooltip.style("top", (event.pageY-10)+"px").style("left",(event.pageX+10)+"px");})
+                    //.on("mouseout", function(){return $scope.tooltip.style("visibility", "hidden");})
                     .attr("r",0).transition().duration(cpi.ENTER_TRANS_DURATION)
                     .attr("r",function(d) { return (d['current_radius']=r(d[key])); });
 
